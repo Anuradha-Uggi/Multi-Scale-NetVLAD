0055f2a570b9bd6ebcedb30bd9e978269aa2b02f
Anuradha <77561069+Anuradha-Uggi@users.noreply.github.com>
2024-10-25 10:50:58 +0530

    Add files via upload

---
+++ gsv-cities/dataloaders/GSVCitiesDataloader.py
@@ -0,0 +1,205 @@
+import pytorch_lightning as pl
+from torch.utils.data.dataloader import DataLoader
+from torchvision import transforms as T
+import configparser
+from dataloaders.GSVCitiesDataset import GSVCitiesDataset
+from . import PittsburgDataset
+#from . import MapillaryDataset
+from dataloaders.datasets_original_bb import PlaceDataset
+from prettytable import PrettyTable
+import os
+
+IMAGENET_MEAN_STD = {'mean': [0.485, 0.456, 0.406], 
+                     'std': [0.229, 0.224, 0.225]}
+
+VIT_MEAN_STD = {'mean': [0.5, 0.5, 0.5], 
+                'std': [0.5, 0.5, 0.5]}
+
+TRAIN_CITIES = [
+    'Bangkok',
+    'BuenosAires',
+    'LosAngeles',
+    'MexicoCity',
+    'OSL',
+    'Rome',
+    'Barcelona',
+    'Chicago',
+    'Madrid',
+    'Miami',
+    'Phoenix',
+    'TRT',
+    'Boston',
+    'Lisbon',
+    'Medellin',
+    'Minneapolis',
+    'PRG',
+    'WashingtonDC',
+    'Brussels',
+    'London',
+    'Melbourne',
+    'Osaka',
+    'PRS',
+]
+'''
+TRAIN_CITIES = [
+    'Medellin',
+]'''
+
+
+import argparse
+parser = argparse.ArgumentParser(description='MS-NetVlad-gsv')
+parser.add_argument('--config_path', type=str, default='/home/anuradha/NV_GSV_TokyoV3/TestResults_320x240/performance_bb.ini',
+                        help='File name (with extension) to an ini file that stores most of the configuration data')    
+parser.add_argument('--query_file_path', type=str, default='/home/anuradha/NV_GSV_TokyoV3/dataset_imagenames/nordland_imageNames_query.txt',
+                        help='qry  dataset folder')                    
+parser.add_argument('--index_file_path', type=str, default='/home/anuradha/NV_GSV_TokyoV3/dataset_imagenames/nordland_imageNames_index.txt',
+                        help='Full path (with extension) to a text file that stores the save location and name of all images in the dataset folder')
+parser.add_argument('--dataset_root_dir', type=str, default='/home/anuradha/MyResearch@Sindhu/DSMAC/',
+                        help='If the files in dataset_file_path are relative, use dataset_root_dir as prefix.')
+parser.add_argument('--ground_truth_path', type=str, default='/home/anuradha/NV_GSV_TokyoV3/dataset_imagenames/gts/nordland.npz',
+                        help='If the files in dataset_file_path are relative, use dataset_root_dir as prefix.')
+opt = parser.parse_args()
+
+configfile = opt.config_path
+assert os.path.isfile(configfile)
+config = configparser.ConfigParser()
+config.read(configfile)
+
+class GSVCitiesDataModule(pl.LightningDataModule):
+    def __init__(self,
+                 batch_size=32,
+                 img_per_place=4,
+                 min_img_per_place=4,
+                 shuffle_all=False,
+                 image_size=(480, 640),
+                 num_workers=4,
+                 show_data_stats=True,
+                 cities=TRAIN_CITIES,
+                 mean_std=IMAGENET_MEAN_STD,
+                 batch_sampler=None,
+                 random_sample_from_each_place=True,
+                 val_set_names=['nordland']
+                 ):
+        super().__init__()
+        self.batch_size = batch_size
+        self.img_per_place = img_per_place
+        self.min_img_per_place = min_img_per_place
+        self.shuffle_all = shuffle_all
+        self.image_size = image_size
+        self.num_workers = num_workers
+        self.batch_sampler = batch_sampler
+        self.show_data_stats = show_data_stats
+        self.cities = cities
+        self.mean_dataset = mean_std['mean']
+        self.std_dataset = mean_std['std']
+        self.random_sample_from_each_place = random_sample_from_each_place
+        self.val_set_names = val_set_names
+        self.save_hyperparameters() # save hyperparameter with Pytorch Lightening
+
+        self.train_transform = T.Compose([
+            T.Resize(image_size, interpolation=T.InterpolationMode.BILINEAR),
+            T.RandAugment(num_ops=3, interpolation=T.InterpolationMode.BILINEAR),
+            T.ToTensor(),
+            T.Normalize(mean=self.mean_dataset, std=self.std_dataset),
+        ])
+
+        self.valid_transform = T.Compose([
+            T.Resize(image_size, interpolation=T.InterpolationMode.BILINEAR),
+            T.ToTensor(),
+            T.Normalize(mean=self.mean_dataset, std=self.std_dataset)])
+
+        self.train_loader_config = {
+            'batch_size': self.batch_size,
+            'num_workers': self.num_workers,
+            'drop_last': False,
+            'pin_memory': True,
+            'shuffle': self.shuffle_all}
+
+        self.valid_loader_config = {
+            'batch_size': self.batch_size,
+            'num_workers': self.num_workers//2,
+            'drop_last': False,
+            'pin_memory': True,
+            'shuffle': False}
+
+    def setup(self, stage):
+        if stage == 'fit':
+            # load train dataloader with reload routine
+            self.reload()
+
+            # load validation sets (pitts_val, msls_val, ...etc)
+            self.val_datasets = []
+            for valid_set_name in self.val_set_names:
+                if valid_set_name.lower() == 'pitts30k_test':
+                    self.val_datasets.append(PittsburgDataset.get_whole_test_set(
+                        input_transform=self.valid_transform))
+                elif valid_set_name.lower() == 'pitts30k_val':
+                    self.val_datasets.append(PittsburgDataset.get_whole_val_set(
+                        input_transform=self.valid_transform))
+                elif valid_set_name.lower() == 'msls_val':
+                    self.val_datasets.append(MapillaryDataset.MSLS(
+                        input_transform=self.valid_transform))
+                elif valid_set_name.lower() == 'nordland':
+                        self.val_datasets.append(PlaceDataset(opt.query_file_path, opt.index_file_path, opt.dataset_root_dir, opt.ground_truth_path, config['feature_extract']))
+                else:
+                    print(
+                        f'Validation set {valid_set_name} does not exist or has not been implemented yet')
+                    raise NotImplementedError
+            if self.show_data_stats:
+                self.print_stats()
+
+    def reload(self):
+        self.train_dataset = GSVCitiesDataset(
+            cities=self.cities,
+            img_per_place=self.img_per_place,
+            min_img_per_place=self.min_img_per_place,
+            random_sample_from_each_place=self.random_sample_from_each_place,
+            transform=self.train_transform)
+                  
+
+    def train_dataloader(self):
+        self.reload()
+        return DataLoader(dataset=self.train_dataset, **self.train_loader_config)
+
+    def val_dataloader(self):
+        val_dataloaders = []
+        for val_dataset in self.val_datasets:
+            val_dataloaders.append(DataLoader(
+                dataset=val_dataset, **self.valid_loader_config))
+        return val_dataloaders
+
+    def print_stats(self):
+        print()  # print a new line
+        table = PrettyTable()
+        table.field_names = ['Data', 'Value']
+        table.align['Data'] = "l"
+        table.align['Value'] = "l"
+        table.header = False
+        table.add_row(["# of cities", f"{len(TRAIN_CITIES)}"])
+        table.add_row(["# of places", f'{self.train_dataset.__len__()}'])
+        table.add_row(["# of images", f'{self.train_dataset.total_nb_images}'])
+        print(table.get_string(title="Training Dataset"))
+        print()
+
+        table = PrettyTable()
+        table.field_names = ['Data', 'Value']
+        table.align['Data'] = "l"
+        table.align['Value'] = "l"
+        table.header = False
+        for i, val_set_name in enumerate(self.val_set_names):
+            table.add_row([f"Validation set {i+1}", f"{val_set_name}"])
+        # table.add_row(["# of places", f'{self.train_dataset.__len__()}'])
+        print(table.get_string(title="Validation Datasets"))
+        print()
+
+        table = PrettyTable()
+        table.field_names = ['Data', 'Value']
+        table.align['Data'] = "l"
+        table.align['Value'] = "l"
+        table.header = False
+        table.add_row(
+            ["Batch size (PxK)", f"{self.batch_size}x{self.img_per_place}"])
+        table.add_row(
+            ["# of iterations", f"{self.train_dataset.__len__()//self.batch_size}"])
+        table.add_row(["Image size", f"{self.image_size}"])
+        print(table.get_string(title="Training config"))
